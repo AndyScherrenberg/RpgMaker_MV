a0605a560eb8b560e9b80c7e83ebbbab5805c11d
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2015-11-06 19:39:28 +0100

    * V1.2:  * Added support for GameVariables  * Added support for LevelVariables  * Fixed problem with the randomizer.  * Added a way to save the levelvariables in the save.    this is optional to use.  * Bug fixed: Max not working  * Bug fixed: Same not working  * Optimized script when using other scripts.  * Bug fix: level smaller then min level  * bug fix: level bigger then max level  * bug fix what happens when a string gets loaded.  * Added setLevelVar plugin command  * Added getLevelVar plugin command  * Extended Helpfile  * Removed problem when loading in a string.
     * V1.1
     * Added debug options. Set the param to 1 to get debug
       messages in your console.
     * Made start for extra variable support
     * Support for extra variables in the regex
     * Function created that handles regex.
     * GetValue created, handles what the regex returns.
     * setUsePartyLeader plugin command added.
     * Added minmonster parameter.

--- Frysning_Leveler.js
+++ Frysning_Leveler.js
@@ -3,7 +3,7 @@
 	Imported.Frysning_Leveler = true;
 
 /*:
- * @plugindesc Change level of monsters. So you wont feel overpowerd
+ * @plugindesc V1.2: Change level of monsters. So you wont feel overpowerd
  * Supports a lot of customization!
  * @author Frysning
 
@@ -62,11 +62,24 @@
  * @desc The max level a monster can be. 
  * @default 99
 
+ * @param minMonster
+ * @desc The min level a monster can be. 
+ * @default 1
+
  * @param usePartyLeader
  * @desc Set this to 1 if you want that the samelevel of the monsters
  * is that of the leader. 0 = highest level in party
  * @default 0
 
+ * @param UseSave
+ * desc If you want to save the level variables. in your save file.
+ * if it is not turned on then you always need to reset your values.
+ * default 0
+
+ * @param DebugModo
+ * @desc Turn this one and when the battle starts you will see 
+ * the stats in the console. Set it to false for release!
+ * default 0
 
  * @help
  * This script is free to use for non-commercial. 
@@ -138,13 +151,110 @@
  * Well that is not needed! The stats you did not define will
  * be based on the global values! 
 
+ * I want to use variables, is this possible?
+ * Yes this is possible from version 1.2.
+
+ * You can use 2 type of variables the gameVariables or levelVariable
+ * To use the gamevariables:
+
+ * Example: 
+ * <level>
+ * min 1
+ * max 10
+ * hp gameVariable 1
+ * mp 2
+ * atk 5
+ * exp 4
+ * </level>
+
+ * This will create the monster of between level 1 and 10. 
+ * The hp increasing will be based on the gameVariable. If the 
+ * game variable is not set, excisting or useable the stat will be -1! 
+
+ * levelVariables are little different to use. 
+ * to set use the plugin command setLevelVar 
+ * This expects 2 variables after. The first one is the key
+ * the second one is the value;
+ * Example:
+ * setLevelVar 1 25
+ * will set LevelVar1 on a value of 25.
+
+ * How to use in noteTag:
+ * Example: 
+ * <level>
+ * min 1
+ * max 10
+ * hp gameVariable 1
+ * mp levelVariable 1
+ * atk 5
+ * exp 4
+ * </level>
+
+ * This will create the monster of between level 1 and 10. 
+ * The hp increasing will be based on the gameVariable.
+ * the mp increasing will be based on the levelVariable in this case 25
+ * If the levelvariable is not set it will return 0; 
+
+ * By default these variables will not get saved. When you save your game.
+ * But you can save them by turning on the parameter.
+
+ * Change Log:
+
+ * V1.2:
+ * Added support for GameVariables
+ * Added support for LevelVariables
+ * Fixed problem with the randomizer.
+ * Added a way to save the levelvariables in the save.
+   this is optional to use.
+ * Bug fixed: Max not working
+ * Bug fixed: Same not working
+ * Optimized script when using other scripts.
+ * Bug fix: level smaller then min level
+ * bug fix: level bigger then max level
+ * bug fix what happens when a string gets loaded. 
+ * Added setLevelVar plugin command
+ * Added getLevelVar plugin command
+ * Extended Helpfile
+ * Removed problem when loading in a string. 
+
+ * V1.1
+ * Added debug options. Set the param to 1 to get debug
+   messages in your console.
+ * Made start for extra variable support
+ * Support for extra variables in the regex
+ * Function created that handles regex.
+ * GetValue created, handles what the regex returns. 
+ * setUsePartyLeader plugin command added.
+ * Added minmonster parameter.
+
+ * V1.0 
+ * First release version.
+
 */
-
-
 	var Frysning_Leveler = Frysning_Leveler || {};
 	Frysning_Leveler.Parameters = PluginManager.parameters('Frysning_Leveler');
 	
 	Frysning_Leveler.paramNames = ["hp", "mp", "atk", "def", "matck", "mdef", "agi", "luck", "gold", "exp"];
+	Frysning_Leveler.LevelVars = [];
+
+	Frysning_Leveler.setUsePartyLeader= function(use){
+			Frysning_Leveler.Parameters["usePartyLeader"] = parseInt(use);
+	}
+
+	/*Frysning_Leveler.setStat= function(stat){
+			Frysning_Leveler.Parameters[stat +"_stat"] = variable;
+	}*/
+
+
+	Game_Interpreter.prototype.pluginCommand = function(command, args) {
+		if (command === "setUsePartyLeader")
+			Frysning_Leveler.setUsePartyLeader(args[0]);
+		if (command === "setLevelVar")
+			Frysning_Leveler.setLevelVar(args[0],args[1]);
+		if (command === "getLevelVar")
+			Frysning_Leveler.getLevelVar(args[0]);
+	};
+
 
 	Game_BattlerBase.prototype.Leveled_Param = function(paramId)
 	{
@@ -162,16 +272,52 @@
 
 	Frysning_Leveler.param = Game_BattlerBase.prototype.param;
 	Game_BattlerBase.prototype.param = function(paramId) {
-		val = Frysning_Leveler.param.apply(this, arguments);
-
-		if (this instanceof Game_Enemy)	{
-			bval = this.Leveled_Param(paramId);
-			val =( bval > 0) ? bval : val;
-
-			console.log(this.name() +" Stat: " + Frysning_Leveler.paramNames[paramId] + " :" + val );
+		
+		val =-1;
+		if (this.isEnemy()) {
+			val = this.Leveled_Param(paramId);
 		}
+
+		if (val < 0){
+			val = Frysning_Leveler.param.apply(this, arguments);
+		}
+
 		return val;
 	};
+	
+	Frysning_Leveler.setLevelVar = function(key, value){
+			Frysning_Leveler.LevelVars[key] = value;
+	}
+
+
+	Frysning_Leveler.getLevelVar =function(key){
+		 if (typeof Frysning_Leveler.LevelVars[key] === 'undefined') return 0;
+		return Frysning_Leveler.LevelVars[key];
+	}
+
+	Frysning_Leveler.getValue= function(value){
+		c_value=0;
+		if(value.contains('gameVariable'))	{
+			temp = value.split(' ');
+			b = parseInt(temp[1]);
+			c_value = $gameVariables.value(b);
+		}
+		else if (value.contains('levelVariable')){
+			temp = value.split(' ');
+			b = parseInt(temp[1]);
+			c_value = 	Frysning_Leveler.getLevelVar(b);
+		}
+		else 
+			c_value = value;
+		return c_value;
+
+	}
+
+	Frysning_Leveler.createRegex = function(stat){
+		cur = "\\b(?:"+stat+")(?:[ ]*)(gameVariable \\d*|levelVariable \\d*|\\d*)";
+		pattern = new RegExp(cur,"i");
+		return pattern
+	}
 
 	Game_Enemy.prototype.Level = function() {
 	
@@ -188,15 +334,20 @@
 			this.level_param[i] =-1;
 		}
 
+		if (Frysning_Leveler.Parameters["DebugModo"] == 1){
+			this.old_values = []
+			this.increasing_stats = [];
+
+			for( i= 0; i <= 7; i++){	
+				this.old_values[i] = this.param(i)
+			}
+			this.old_values[8] = this.gold();
+			this.old_values[9] = this.exp()
+		}
+
 		this.NoteArray = this.NoteArray[1];
-
-		cur = "\\b(?:min)(?:[ ]*)([0-9]*)";
-		pattern = new RegExp(cur,"i");
-		this.min = this.NoteArray.match(pattern);//CurrentLEvel
-
-		cur = "\\b(?:max)(?:[ ]*)([0-9]*)";
-		pattern = new RegExp(cur,"i");
-		this.max = this.NoteArray.match(pattern);
+		this.min = this.NoteArray.match(Frysning_Leveler.createRegex("min"));
+		this.max = this.NoteArray.match(Frysning_Leveler.createRegex("max"));
 
 		cur = "\\b(?:same)";
 		pattern = new RegExp(cur,"i");
@@ -207,78 +358,130 @@
 		this.showlevel = this.NoteArray.match(pattern)
 
 		if (typeof 	this.min === 'undefined' || this.min == null)
-			this.min = 1;
+			this.min =  parseInt(Frysning_Leveler.Parameters["minMonster"])
 		else 
-			this.min = parseInt(this.min[1])
+		{
+			this.min =  parseInt(Frysning_Leveler.getValue(this.min[1]))
+			if (this.min === 0) {
+				this.min =  parseInt(Frysning_Leveler.Parameters["minMonster"])}
+		}
 
 		if (typeof 	this.max === 'undefined'|| this.max == null){
 			this.usemax = false;
 			this.max =  parseInt(Frysning_Leveler.Parameters["maxMonster"])}
 		else {
 			this.usemax  = true;
-			this.max = parseInt(this.max[1])
+			this.max = parseInt(Frysning_Leveler.getValue(this.max[1]))
+	
+			if (this.max === 0) {
+				this.max =  parseInt(Frysning_Leveler.Parameters["maxMonster"])
+			}
 		}
 
-		if (typeof this.same !== 'undefined' && this.same != null)
+		if (typeof this.same !== 'undefined' && this.same != null){
 			this.same = true;
-		else this.same = false;
+		}
+		else{ this.same = false;}
 
-				if (typeof this.showlevel !== 'undefined' && this.showlevel != null)
+		if (typeof this.showlevel !== 'undefined' && this.showlevel != null){
 			this.showlevel = true;
-		else this.showlevel = false;
+		}
+		else {
+			this.showlevel = false;
+		}
 
 
 		if (this.same == true){
-
 			this.same_level =0;
-
-			console.log(Frysning_Leveler.Parameters["usePartyLeader"])
 			if (Frysning_Leveler.Parameters["usePartyLeader"] == 1){
-				console.log("HGello");
 					this.same_level = $gameParty.leader()._level;
 			}
 			else
 				this.same_level =  $gameParty.highestLevel();
 
-
 			if (this.usemax == false)
-				this.level =this.same_level
+				this.level_frys =this.same_level
 			else{	
-			if (this.max > this.same_level){
-				this.level = this.same_level;
+				if (this.max > this.same_level){
+					this.level_frys = this.same_level;
+				}
+				else
+					this.level_frys = this.max;
+				}	
 			}
-			else
-				this.level = this.max;
-			}	
-		}
 		else{
-			this.level = Math.floor((Math.random() *( this.max - this.min)) + this.min);
+
+			this.level_frys = Math.floor( Math.random() * (this.max -  this.min) +1);
+			this.level_frys += this.min
+		
+			if(this.level_frys < this.min)
+				this.level_frys = this.min
+		
+			if(this.level_frys > this.max)
+				this.level_frys = this.max
+		
 		}
 
 		for( i= 0; i <= 9; i++){
 			this.IncreaseStat(i);
 		}
 
+		if (Frysning_Leveler.Parameters["DebugModo"] == 1)
+		{
+			this.new_values=[];
+			for( i= 0; i <= 7; i++){	
+				this.new_values[i] = this.param(i)
+			}
+			this.new_values[8] = this.gold();
+			this.new_values[9] = this.exp()
+		
+			console.log('Information for: ' + this.name());
+			console.log('Min Level: '+ this.min+'  Max Level: '+ this.max+' Choosen Level: '+ this.level_frys);
+
+  			console.log('Smaller then 0 means not used' )	
+			console.log('Params: Hp, Mp, Atk, Def, Matk, mDef, agi, luck, gold,exp' )	
+			console.log('Current Params: ' )
+			console.log(this.level_param)
+			console.log('Database Values: ')
+			console.log(this.old_values)
+			console.log('Increasing each param: ');
+			console.log(this.increasing_stats )
+
+			console.log('Eventually: ');
+			console.log(this.new_values )
+		}
 	};
 
 	Game_Enemy.prototype.IncreaseStat = function(type) {
 		var s = Frysning_Leveler.paramNames[type]; 
-		var f = "\\b(?:"+s+")(?:[ ]*)([0-9%]*)";
-		
-		pattern = new RegExp(f,"i");
-		stat = this.NoteArray.match(pattern);
-
+		stat = this.NoteArray.match(Frysning_Leveler.createRegex(s));
 
 		if (stat == null || typeof stat === 'undefined'){
 			stat = Frysning_Leveler.Parameters[s];
 		}
 		else{
-			stat = stat[1];
+			stat = Frysning_Leveler.getValue(stat[1])
 		}
 
-	 	var increasePercent = (stat.contains('%')) ? true :false;
-	 	var increasing = stat.split('%')[0]
+		if (Frysning_Leveler.Parameters["DebugModo"] == 1){
+			this.increasing_stats[i] = stat;
+		}
+	
+		var increasePercent= false;
+	 	var increasing = stat;
 		var baseStat = this.param(type);
+
+		if (isNaN(stat)){
+		 	increasePercent = (stat.contains('%')) ? true :false;
+		 	if(increasePercent){
+		 	increasing = stat.split('%')[0]
+		 	console.log(increasing);}
+		}
+
+		if (isNaN(increasing) || increasing == "") {
+				this.level_param[type] = -1;
+				return
+			};
 
 		if(type == 8)
 			baseStat= this.gold();
@@ -286,19 +489,16 @@
 			baseStat = this.exp();
 
 		if (increasePercent === true){
-
 			if(increasing.length == 2)
 				increasefloaty =  parseFloat(1+"." +increasing);
 			if(increasing.length == 1)
 				increasefloaty =  parseFloat(1+".0" +increasing);
 			if(increasing.length == 3)
 				increasefloaty =  parseFloat(increasing[0]+"." +increasing[1] + "" + increasing[2]);
-
 			increasing = increasefloaty
 		}
 
-
-		for(j = this.min; j< this.level; j++){
+		for(j = this.min; j< this.level_frys; j++){
 			if (increasePercent === true){
 				baseStat = baseStat *  increasing;
 			}
@@ -316,11 +516,8 @@
 		}
 	}
 
-
-
-
 	Game_Enemy.prototype.name = function() {
-	    return this.originalName() + (this._plural ? this._letter : '') + " " + (this.showlevel ?  ' Lv: ' + this.level : '');
+	    return this.originalName() + (this._plural ? this._letter : '') + " " + (this.showlevel ?  ' Lv: ' + this.level_frys : '');
 	};
 
 	Game_Enemy.prototype.exp = function() {
@@ -342,8 +539,27 @@
 
 	Frysning_Leveler.setup = Game_Troop.prototype.setup;
 	Game_Troop.prototype.setup  = function(troopId) {
-		    Frysning_Leveler.setup.apply(this, arguments);
-			this._enemies.forEach(function(member) {
-				member.Level();
-	 	});
+
+		Frysning_Leveler.setup.apply(this, arguments);
+		
+		this._enemies.forEach(function(member) {
+			member.Level();});
 	};
+
+
+	if (Frysning_Leveler.Parameters["UseSave"] == 1){
+		Frysning_Leveler.makeSaveContents = DataManager.makeSaveContents;
+		DataManager.makeSaveContents = function() {  
+		 	var contents = Frysning_Leveler.makeSaveContents.apply(this, arguments);
+		    contents.level_vars  =  Frysning_Leveler.LevelVars;
+		    return contents;
+		};
+
+
+		Frysning_Leveler.extractSaveContents = DataManager.extractSaveContents;
+		DataManager.extractSaveContents = function(contents) {
+		  	Frysning_Leveler.extractSaveContents.apply(this, arguments);
+		     Frysning_Leveler.LevelVars = contents.level_vars;  
+		};
+	}
+
